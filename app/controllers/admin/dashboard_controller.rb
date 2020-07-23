ff6fa011a09a32e160d0400c4fa72bb9f1ed313f
Bryan's Mac <bryanpgomes@gmail.com>
2020-07-23 15:50:53 -0400

    Category count now works, working on stretch to add bootstrap elements

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,6 @@ class Admin::DashboardController < ApplicationController
 
   def show
     @product_count = Product.count
-    @category_count = Product.count
+    @category_count = Category.count
   end
 end

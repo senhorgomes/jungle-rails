7a421677a1f490a4f5b3f04fbc8b2706b9d7b8b7
Bryan's Mac <bryanpgomes@gmail.com>
2020-07-23 15:46:55 -0400

    Product count is working, now implementing category count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   before_filter :authenticate
 
   def show
+    @product_count = Product.count
+    @category_count = Product.count
   end
 end

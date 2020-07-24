0a113d950f6b197ac73ff628a1aadc08ae5b19ca
Bryan Gomes <bryanpgomes@gmail.com>
2020-07-24 20:04:44 +0000

    Added login,logout and signup button to nav bar. Feature complete

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,14 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  # Routes for logging in
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+  # Routes for creating new users
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]

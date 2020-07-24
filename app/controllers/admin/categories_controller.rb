b347a33788700af482fca99ad6a777a6f2458b31
Bryan Gomes <bryanpgomes@gmail.com>
2020-07-24 00:20:29 +0000

    Controller has been generated.Starting with index page

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,35 @@
+class Admin::CategoriesController < ApplicationController
+  before_filter :authenticate
+
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :name,
+      :description,
+      :category_id,
+      :quantity,
+      :image,
+      :price
+    )
+  end
+  
+end

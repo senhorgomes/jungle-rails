aa46c1a75824c959f3e3a926da93e389062816e1
Bryan Gomes <bryanpgomes@gmail.com>
2020-07-28 20:41:32 +0000

    All tests implemented, and all pass. Merging branches

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    it "verifies the presence of the new product" do
+    it "verifies that a product with all four fields set, will save" do
       newCategory = Category.create name: "Cars"
       newProduct = Product.create name: "Honda Civic", price: 10000, quantity: 1, category: newCategory
       expect(newProduct).to be_valid
@@ -13,6 +13,27 @@ RSpec.describe Product, type: :model do
       newProduct = Product.create name: nil, price: 10000, quantity: 1, category: newCategory
       
       expect(newProduct.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it "cannot be created without price" do
+      newCategory = Category.create name: "Cars"
+      newProduct = Product.create name: "Honda Civic", price: nil, quantity: 1, category: newCategory
+      
+      expect(newProduct.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it "cannot be created without quantity" do
+      newCategory = Category.create name: "Cars"
+      newProduct = Product.create name: "Honda Civic", price: 10000, quantity: nil, category: newCategory
+      
+      expect(newProduct.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+    it "cannot be created without category" do
+      newCategory = Category.create name: "Cars"
+      newProduct = Product.create name: "Honda Civic", price: 10000, quantity: 1, category: nil
+      
+      expect(newProduct.errors.full_messages).to include("Category can't be blank")
     end
   end
 end

552a283a28bc432d96ef962a7f176fa9a2bf3ff1
Bryan Gomes <bryanpgomes@gmail.com>
2020-07-28 20:30:37 +0000

    Adding more validations

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    # validation tests/examples here
+    it "verifies the presence of the new product" do
+      newCategory = Category.create name: "Cars"
+      newProduct = Product.create name: "Honda Civic", price: 10000, quantity: 1, category: newCategory
+      expect(newProduct).to be_valid
+    end
+
+    it "cannot be created without name" do
+      newCategory = Category.create name: "Cars"
+      newProduct = Product.create name: nil, price: 10000, quantity: 1, category: newCategory
+      
+      expect(newProduct.errors.full_messages).to include("Name can't be blank")
+    end
   end
 end

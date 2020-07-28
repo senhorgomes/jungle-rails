37e8d9e22bd55e921ac5a17f3233dd0a87a8ccf1
Bryan Gomes <bryanpgomes@gmail.com>
2020-07-28 21:19:54 +0000

    Adding validation to user input before saving

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ActiveRecord::Base
 
   has_secure_password
   
+  validates :name, presence: true
+  validates :password_digest, length: { minimum: 6}
+  validates :email, presence: true
+
+  before_save :downcase_email
+
+  def downcase_email
+    self.email.downcase!
+  end
 end

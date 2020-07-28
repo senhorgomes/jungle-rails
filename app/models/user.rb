62216aa5f36f1330fd46caa690d2a156613107a6
Bryan Gomes <bryanpgomes@gmail.com>
2020-07-28 22:07:48 +0000

    Attempting first spec, had to change password_digest to password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,15 @@ class User < ActiveRecord::Base
   has_secure_password
   
   validates :name, presence: true
-  validates :password_digest, length: { minimum: 6}
+  validates :password, length: { minimum: 6}
   validates :email, presence: true
 
   before_save :filtered_email
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase!)
+    user && user.authenticate(password)
+  end
 
   def filtered_email
     self.email = self.email.strip.downcase!
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Validations' do
-    it "verifies that a product with all four fields set, will save" do
-      newUser = User.create name: "Cars", password_digest: "qwerty", email: "     Atl@.com"
-      # expect(newProduct).to be_valid
-      puts newUser.email
+    it "that user is created" do
+      newUser = User.create name: "Kate Mint", password: "qwerty", email: "kmint@email.com"
+      
+      expect(newUser).to be_valid
     end
   end
 end

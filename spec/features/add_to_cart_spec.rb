4263c053993081bf95a12a8e129e93be5cc12512
Bryan Gomes <bryanpgomes@gmail.com>
2020-07-30 02:42:05 +0000

    Test passes. Had to remove before_filter for many pages, as visitors should still be able to place order

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts", type: :feature, js: true do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    1.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They can add product to cart" do
+    #ACT
+    visit root_path
+    click_button "Add"
+    #DEBUG
+    save_screenshot
+    #VERIFY
+    expect(page).to have_content("My Cart (1)")
+  end
+end
